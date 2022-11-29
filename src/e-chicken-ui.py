9968a3d92d2850b0eba7b55c8c90c54319e869b7
Ronny Friedland <mail@ronnyfriedland.de>
2022-11-29 19:23:04 +0100

    Introduce pylint, increase code quality

--- src/e-chicken-ui.py
+++ src/e-chicken-ui.py
@@ -1,46 +1,57 @@
+"""
+Provides html ui
+"""
 from flask import Flask,render_template, request,json
-
-import RPi.GPIO as GPIO
+from RPi import GPIO
 
 
 app = Flask(__name__, template_folder='content')
 
 @app.route('/')
 def index():
-  return render_template('index.html')
+    """
+    Handle index page
+    """
+    return render_template('index.html')
 
 
 @app.route('/pin', methods=['PUT'])
 def toggle():
-  pin =  request.form['pin']
-  state = request.form['state']
+    """
+    Toggle pin status
+    """
+    pin =  request.form['pin']
+    state = request.form['state']
 
-  try:
-      GPIO.setmode(GPIO.BCM)
-      GPIO.setup(int(pin), GPIO.OUT)
+    try:
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(int(pin), GPIO.OUT)
 
-      if(state == "HIGH"):
-        GPIO.output(int(pin), GPIO.HIGH)
-        new_state = "HIGH"
-      if(state == "LOW"):
-        GPIO.output(int(pin), GPIO.LOW)
-        new_state = "LOW"
-  finally:
-      print("pin %d updated to state %s".format(pin, state))
+        if state == "HIGH":
+            GPIO.output(int(pin), GPIO.HIGH)
+            new_state = "HIGH"
+        if state == "LOW":
+            GPIO.output(int(pin), GPIO.LOW)
+            new_state = "LOW"
+    finally:
+        print(f"pin {pin} updated to state {state}")
 
-  return json.dumps({'status':'OK','pin':pin,'state':new_state})
+    return json.dumps({'status':'OK','pin':pin,'state':new_state})
 
 @app.route('/pin/<pin>', methods=['GET'])
 def status(pin):
-  try:
-      GPIO.setmode(GPIO.BCM)
-      GPIO.setup(int(pin), GPIO.OUT)
+    """
+    Retrieve pin status
+    """
+    try:
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(int(pin), GPIO.OUT)
 
-      is_on = GPIO.input(int(pin))
-  finally:
-      print("pin %d state returned".format(pin))
+        is_on = GPIO.input(int(pin))
+    finally:
+        print(f"pin {pin} state returned")
 
-  return json.dumps({'status':'OK','pin':pin,'state': "HIGH" if is_on else "LOW" })
+    return json.dumps({'status':'OK','pin':pin,'state': "HIGH" if is_on else "LOW" })
 
 
 if __name__ == '__main__':
